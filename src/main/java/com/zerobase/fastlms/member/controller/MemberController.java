aab27c43f7a6002cb935871a85930f3e4b74e61a
haden <80521474+thdefn@users.noreply.github.com>
2023-03-13 13:35:30 +0900

    jpa를 통한 db 저장

--- src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
+++ src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.zerobase.fastlms.member.service.MemberService;
 import com.zerobase.fastlms.member.model.MemberInput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,18 +16,17 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    //@RequestMapping(value = "/member/register", method = RequestMethod.GET)
     @GetMapping("/member/register")
     public String register() {
         return "member/register";
     }
 
-    //register.html에서 form이 submit 될 때 작동
-    //@RequestMapping(value = "/member/register", method = RequestMethod.POST)
     @PostMapping("/member/register")
-    public String registerSubmit(HttpServletRequest request,
+    public String registerSubmit(Model model, //클라이언트에게 데이터를 내리기 위해 사용되는 인터페이스
+                                 HttpServletRequest request,
                                  MemberInput parameter) {
         boolean result = memberService.register(parameter);
+        model.addAttribute("result",result);
         return "member/register-complete";
     }
 }
--- src/main/java/com/zerobase/fastlms/member/service/MemberServiceImpl.java
+++ src/main/java/com/zerobase/fastlms/member/service/MemberServiceImpl.java
@@ -7,14 +7,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service //빈으로 등록
 public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
+    /**
+     * 회원 가입
+     */
     @Override
     public boolean register(MemberInput parameter) {
+
+        // jpa의 아이디로 엔티티 찾기 메서드
+        Optional<Member> optionalMember = memberRepository.findById(parameter.getUserId());
+        if(optionalMember.isPresent()){
+            // 현재 userId에 해당하는 데이터 존재
+            return false;
+        }
+
         Member member = new Member();
         member.setUserId(parameter.getUserId());
         member.setUserName(parameter.getUserName());
@@ -24,6 +36,6 @@ public class MemberServiceImpl implements MemberService{
 
         memberRepository.save(member); // 기존에 있는 데이터는 자동 업데이트
 
-        return false;
+        return true;
     }
 }
